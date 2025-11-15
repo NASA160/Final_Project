b80952646e0e3ad3c3f4b1d4f102f6149900faa7
Kyle McPherson <ksmcp9@gmail.com>
2025-11-15 18:13:15 -0500

    More stuff

--- player.py
+++ player.py
@@ -1,3 +1,5 @@
+import math
+
 import pygame
 
 from settings import *
@@ -5,24 +7,26 @@ from settings import *
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load('PlayerSprites/images.jpg').convert_alpha()
+        self.image = pygame.image.load('PlayerSprites/white_square.png').convert_alpha()
         self.rect = self.image.get_rect()
         self.speed = 10
-        self.direction = pygame.Vector2(0,0)
+        self.direction = pygame.Vector2()
 
     def inputs(self):
         self.keys = pygame.key.get_pressed()
         self.direction.y = int(self.keys[pygame.K_DOWN]) - int(self.keys[pygame.K_UP])
         self.direction.x = int(self.keys[pygame.K_RIGHT]) - int(self.keys[pygame.K_LEFT])
         print(self.direction)
-        # self.direction = self.direction.normalize()
+        # TODO optimize normilization
+        # self.direction.normalize_ip()
+        # if math.fabs(self.direction.y) and math.fabs(self.direction.x):
+        #     self.direction.y *= (1/math.sqrt(2))
+        #     self.direction.x *= (1/math.sqrt(2))
 
 
     def move(self, dt):
         self.rect.x += self.direction.x * self.speed
         self.rect.y += self.direction.y * self.speed
-        # TODO fix normilization
-        # self.direction.normalize()
 
     def update(self, dt):
         self.inputs()
