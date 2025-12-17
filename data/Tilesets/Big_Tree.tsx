4ac5286b3bd07f78673d7541d6e4bb3c164e97f2
Kyle McPherson <ksmcp9@gmail.com>
2025-12-16 20:40:17 -0500

    Made tilesets accessable

--- data/Tilesets/Big_Tree.tsx
+++ data/Tilesets/Big_Tree.tsx
@@ -10,31 +10,31 @@
  <tile id="5">
   <image source="../Graphics/objects/Small_Tree.png" width="28" height="40"/>
  </tile>
- <tile id="7">
-  <image source="../../../../../Downloads/Pine.png" width="30" height="44"/>
+ <tile id="16">
+  <image source="../Graphics/objects/Big_Dis_Ice_Tree.png" width="47" height="48"/>
  </tile>
- <tile id="8">
-  <image source="../../../../../Downloads/Ice_Pine.png" width="30" height="45"/>
+ <tile id="17">
+  <image source="../Graphics/objects/Big_Dis_Snow_Tree.png" width="47" height="48"/>
  </tile>
- <tile id="9">
-  <image source="../../../../../Downloads/Snow_Pine.png" width="30" height="44"/>
+ <tile id="18">
+  <image source="../Graphics/objects/Big_dis_Tree.png" width="47" height="48"/>
  </tile>
- <tile id="10">
-  <image source="../../../../../Downloads/Dis_Tree.png" width="24" height="45"/>
+ <tile id="19">
+  <image source="../Graphics/objects/Dis_Ice_Tree.png" width="24" height="45"/>
  </tile>
- <tile id="11">
-  <image source="../../../../../Downloads/Dis_Ice_Tree.png" width="24" height="45"/>
+ <tile id="20">
+  <image source="../Graphics/objects/Dis_Snow_Tree.png" width="24" height="45"/>
  </tile>
- <tile id="12">
-  <image source="../../../../../Downloads/Dis_Snow_Tree.png" width="24" height="45"/>
+ <tile id="21">
+  <image source="../Graphics/objects/Dis_Tree.png" width="24" height="45"/>
  </tile>
- <tile id="13">
-  <image source="../../../../../Downloads/Big_dis_Tree.png" width="47" height="48"/>
+ <tile id="22">
+  <image source="../Graphics/objects/Ice_Pine.png" width="30" height="45"/>
  </tile>
- <tile id="14">
-  <image source="../../../../../Downloads/Big_Dis_Ice_Tree.png" width="47" height="48"/>
+ <tile id="23">
+  <image source="../Graphics/objects/Pine.png" width="30" height="44"/>
  </tile>
- <tile id="15">
-  <image source="../../../../../Downloads/Big_Dis_Snow_Tree.png" width="47" height="48"/>
+ <tile id="24">
+  <image source="../Graphics/objects/Snow_Pine.png" width="30" height="44"/>
  </tile>
 </tileset>
